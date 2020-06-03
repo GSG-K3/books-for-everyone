81aa4f09b7e1a3eb7a1b444a63db6da22e2d9b3c
bayanseder <bayansider88@gmail.com>
2020-06-03 19:44:49 +0300

    creat header component & add img file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import BooksImg from '../../Images/books-cartoon1.png'
+import './header.css'
+
+function Header(){
+return (
+    <div className='header_component_main_div'>
+        <div className='header_title_div'>
+            <h1>BOOKS</h1>
+            <p>For Everyone</p>
+        </div>
+        <div className='header_img_div'>
+            <img src={BooksImg} alt='books' />
+        </div>
+    </div>
+);
+}
+
+export default Header
