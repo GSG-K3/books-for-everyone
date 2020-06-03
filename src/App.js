6ecb6efdf5f3d5144b7118c8fa112c016eeddf1c
bayanseder <bayansider88@gmail.com>
2020-06-03 22:42:15 +0300

    modify components path & add search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Common/Header/Header'
+import SearchBar from './components/Common/Search/Search'
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <SearchBar />
     </div>
   );
 }
