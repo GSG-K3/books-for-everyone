32973aec1383622ab48d0ab080ad2dfba98be11e
bayanseder <bayansider88@gmail.com>
2020-06-04 16:17:23 +0300

    modify search function & add book info

---
+++ src/components/Layout/BookInfo/BookInfo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function BookInfo(props) { 
+    const {bookDetails}=props
+    console.log(bookDetails)
+    return (
+        <div>
+            <img src={bookDetails.volumeInfo.imageLinks.smallThumbnail} 
+                 alt='book-poster' />
+            <h1>{bookDetails.volumeInfo.title}</h1>
+            <h2>{bookDetails.volumeInfo.authors} </h2>
+            <p>{bookDetails.volumeInfo.publisher}+' '+{bookDetails.volumeInfo.publishedDate} </p>
+            <p>{bookDetails.volumeInfo.description}</p>
+            <a href={bookDetails.volumeInfo.previewLink} >Read this Book</a>
+
+        </div>
+    )
+
+}
+
+export default BookInfo
